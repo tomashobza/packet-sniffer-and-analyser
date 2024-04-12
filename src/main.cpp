dd41f23e268cef5d8ef7dad54cfd1f163bfd1b93
Tomáš Hobza <hobza.tomas@gmail.com>
2024-04-12 15:29:33 +0200

    feat: :sparkles: capture n packets

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,10 @@
 #include "ArgParser.hpp"
 #include "Cmuchator.hpp"
 
-// void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
-// {
-//     printf("Got a packet with length of [%d]\n", header->len);
-//     (void)packet;
-//     (void)args;
-// }
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+    printf("Got a packet with length of [%d]\n", header->len);
+}
 
 int main(int argc, char *argv[])
 {
@@ -17,24 +15,31 @@ int main(int argc, char *argv[])
     (void)argv;
     SnifferOptions options;
 
+    options = ArgParser::parse(argc, argv);
+
+    if (options.help)
+    {
+        ArgParser::help();
+    }
+
     if (!options.interfaceSpecified)
     {
         std::cout << "Interface not specified. Listing available interfaces:" << std::endl;
         Cmuchator::listInterfaces();
     }
 
-    // char errbuf[PCAP_ERRBUF_SIZE];
-    // pcap_t *handle = pcap_open_live("eth0", BUFSIZ, 1, 1000, errbuf);
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *handle = pcap_open_live(options.interface.data(), BUFSIZ, 1, 1000, errbuf);
 
-    // if (handle == nullptr)
-    // {
-    //     fprintf(stderr, "Couldn't open device eth0: %s\n", errbuf);
-    //     return 2;
-    // }
+    if (handle == nullptr)
+    {
+        fprintf(stderr, "Couldn't open device eth0: %s\n", errbuf);
+        return 2;
+    }
 
-    // pcap_loop(handle, 10, got_packet, nullptr);
+    pcap_loop(handle, options.num, got_packet, nullptr);
 
-    // pcap_close(handle); // Close the handle
+    pcap_close(handle);
 
     return EXIT_SUCCESS;
 }
